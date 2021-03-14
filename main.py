d2dd7ecf53ffbaee2dc2aa84d80c3b0f9f2dd71f
fdujwc <fdujwc.cn@gmail.com>
2021-03-14 16:02:18 +0800

    unit cfg test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from astEngine.astGen import astGen
+from testEngine import unitTest
 
 if __name__ == "__main__":
-    file = open("./testEngine/codeTested/test0.py", encoding="utf8")
-    code = file.read()
-    astTree = astGen(code, print = True).astReturn()
+    unitTest()
---
+++ testEngine/__init__.py
@@ -0,0 +1,8 @@
+from staticfg import CFGBuilder
+
+def unitTest():
+    testid = 0
+    testname = 'test{}'.format(testid)
+    code = open('./testEngine/codeTested/{}.py'.format(testname), encoding="utf8").read()
+    cfg = CFGBuilder().build_from_src(testname, code)
+    cfg.build_visual('./testEngine/cfgTested/{}/{}'.format(testname, testname), 'png')
